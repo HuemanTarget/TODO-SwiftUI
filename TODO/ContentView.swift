ae7b0c1f9abbeb2c6d75835e4aa121b7cd72cec9
Joshua Basche <joshuabasche@Joshuas-MacBook-Pro.local>
2021-02-25 14:15:45 -0800

    first commit

---
+++ TODO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TODO
+//
+//  Created by Joshua Basche on 2/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
